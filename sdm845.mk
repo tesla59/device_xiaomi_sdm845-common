2ca8fcbc52b4c1dc5b8af08ff688248dc78db985
PainKiller3 <ninadpatil100@gmail.com>
2019-10-08 17:28:53 +0530

    Revert "sdm845-common: Create system copy of ims-ext-common and add to classpath"
    This reverts commit 53924e2e5c514aaa06f2dd847b569c77b9bcca89.
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -180,10 +180,7 @@ PRODUCT_BOOT_JARS += \
 
 # IMS
 PRODUCT_PACKAGES += \
-    ims-ext-common_system
-
-PRODUCT_BOOT_JARS += \
-    ims-ext-common_system
+    ims-ext-common
 
 # Input
 PRODUCT_COPY_FILES += \
